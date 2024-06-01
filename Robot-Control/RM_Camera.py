5c0f857a85d65a10c4e7e13db07b753c1d2d98d0
SimLabJW <89232609+SimLabJW@users.noreply.github.com>
2024-06-01 19:01:57 +0900

    d

--- Robot-Control/RM_Camera.py
+++ Robot-Control/RM_Camera.py
@@ -14,20 +14,42 @@
 # limitations under the License.
 
 
-import time
-from robomaster import robot
-from robomaster import camera
+# import time
+# from robomaster import robot
+# from robomaster import camera
 
 
-if __name__ == '__main__':
-    ep_robot = robot.Robot()
-    ep_robot.initialize(conn_type="sta")
+# if __name__ == '__main__':
+#     ep_robot = robot.Robot()
+#     ep_robot.initialize(conn_type="sta")
 
-    ep_camera = ep_robot.camera
+#     ep_camera = ep_robot.camera
 
-    # 显示十秒图传
-    ep_camera.start_video_stream(display=True, resolution=camera.STREAM_360P)
-    time.sleep(10)
-    ep_camera.stop_video_stream()
+#     # 显示十秒图传
+#     ep_camera.start_video_stream(display=True, resolution=camera.STREAM_360P)
+#     time.sleep(10)
+#     ep_camera.stop_video_stream()
 
-    ep_robot.close()
+#     ep_robot.close()
+
+
+# import cv2
+# from robomaster import robot
+
+
+# if __name__ == '__main__':
+#     ep_robot = robot.Robot()
+#     ep_robot.initialize(conn_type="sta")
+
+#     ep_camera = ep_robot.camera
+
+#     # 显示200帧图传
+#     ep_camera.start_video_stream(display=False)
+#     for i in range(0, 200):
+#         img = ep_camera.read_cv2_image()
+#         cv2.imshow("Robot", img)
+#         cv2.waitKey(1)
+#     cv2.destroyAllWindows()
+#     ep_camera.stop_video_stream()
+
+#     ep_robot.close()
